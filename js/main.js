0ad0701786fcf09b88e2e906b563930b82025728
sally <sally@sall.ipv6.infinitum.net.mx>
2026-06-13 15:16:25 -0600

    feat(web): style buttons and apply IIFE in the js

--- js/main.js
+++ js/main.js
@@ -1,80 +1,82 @@
-function getComputerChoice() {
+( () => {
+
+  function getComputerChoice() {
+    const options = ["rock", "paper", "scissors"];
+    const indexPosition = Math.floor((Math.random() * options.length));
+    return options[indexPosition];
+  }
+
+  // TODO: Refactorizar getUserChoice() para interacuar con el DOM
+  // Quizá al presionar una opción, ejecutar playGame() y enviar el valor del botón
+  // TODO: Asignar a cada botón la función getUserChoice()
+
+  function getUserChoice() {
   
-  const options = ["rock", "paper", "scissors"];
-  const indexPosition = Math.floor((Math.random() * options.length));
-  return options[indexPosition];
-}
-
-// TODO: Refactorizar getUserChoice() para interacuar con el DOM
-// Quizá al presionar una opción, ejecutar playGame() y enviar el valor del botón
-// TODO: Asignar a cada botón la función getUserChoice()
-function getUserChoice() {
+    const validAnswers = ["rock", "paper", "scissors"];
+    let userInput;
   
-  const validAnswers = ["rock", "paper", "scissors"];
-  let userInput;
+    do {
+      let rawInput = prompt("Select rock, paper or scissors");
+      // If user pressed "cancel", return an empty string
+      userInput = rawInput ? rawInput.toLowerCase() : "";
+    } while (!userInput || !validAnswers.includes(userInput));
 
-  do {
-    let rawInput = prompt("Select rock, paper or scissors");
-    // If user pressed "cancel", return an empty string
-    userInput = rawInput ? rawInput.toLowerCase() : "";
+    return userInput;
+  }
 
-  } while (!userInput || !validAnswers.includes(userInput));
-
-  return userInput;
-}
-
-function playRound(humanChoice, computerChoice) {
+  function playRound(humanChoice, computerChoice) {  
   
   if (humanChoice === computerChoice) return "draw";
+  
+    const winsAgainst = {
 
-  const winsAgainst = {
-    
     rock: "scissors",
     paper: "rock",
     scissors: "paper"
-  };
+    };
 
-  return winsAgainst[humanChoice] === computerChoice ? "human" : "computer";
-
-}
-
-
-
-function playGame() {
-
-  let humanScore = 0;
-  let computerScore = 0;
-
-  while (humanScore < 5 || computerScore < 5) {
-  
-    // TODO: Adaptar para recibir respuesta del DOM
-    // const humanChoice = getUserChoice();
-    // const computerChoice = getComputerChoice();
-
-    const winner = playRound(humanChoice, computerChoice);
-
-    if (winner === "draw") {
-      alert("It's a draw!");
-      i--;
-      continue;
-    }
-
-    if (winner === "human") {
-      alert(`You win this round! ${humanChoice} beats ${computerChoice}`);
-      humanScore++;
-
-    } else {
-      alert(`You lost this round! ${computerChoice} beats ${humanChoice}`);
-      computerScore++;
-    }
+    return winsAgainst[humanChoice] === computerChoice ? "human" : "computer";
   }
 
-  const winnerMessage = humanScore > computerScore
-  ? `You win! Your Score: ${humanScore} | Computer Score: ${computerScore}`
-  : `You lose! Your Score: ${humanScore} | Computer Score: ${computerScore}`
 
-  alert(winnerMessage);
 
-}
 
-// playGame();
+  function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    while (humanScore < 5 || computerScore < 5) {
+  
+      // TODO: Adaptar para recibir respuesta del DOM
+      // const humanChoice = getUserChoice();
+      // const computerChoice = getComputerChoice();
+
+      const winner = playRound(humanChoice, computerChoice);
+
+    
+      if (winner === "draw") {
+        alert("It's a draw!");
+        i--;
+        continue;
+      }
+
+      if (winner === "human") {  
+        alert(`You win this round! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+      } else {
+        alert(`You lost this round! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+      }
+  }
+    const winnerMessage = humanScore > computerScore
+  
+      ? `You win! Your Score: ${humanScore} | Computer Score: ${computerScore}`  
+      : `You lose! Your Score: ${humanScore} | Computer Score: ${computerScore}`
+
+    alert(winnerMessage);
+  }
+
+  // playGame();
+
+})();
