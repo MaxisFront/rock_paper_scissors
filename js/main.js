646bf2cbde087e1c1d486f969376d61f54a9be2b
sally <sally@sall.ipv6.infinitum.net.mx>
2026-06-13 20:09:39 -0600

    style: increase RPS plazone width

--- js/main.js
+++ js/main.js
@@ -1,76 +1,178 @@
-function getComputerChoice() {
+// @ts-check
+
+( () => {
   
-  const options = ["rock", "paper", "scissors"];
-  const indexPosition = Math.floor((Math.random() * options.length));
-  return options[indexPosition];
-}
+  // game logic
+  let userScore = 0;
+  let computerScore = 0;
+  let endGame = false;
 
-function getUserChoice() {
+  // DOM elements
+  const outputText = document.querySelector(".rps__output-text");
+  const userIcon = document.querySelector('[data-icon="user"]')
+  const computerIcon = document.querySelector('[data-icon="computer"]')
+  const userScoreText = document.querySelector('[data-score="user"]')
+  const computerScoreText = document.querySelector('[data-score="computer"]')
+
+
+  const playButtons = document.querySelectorAll(".bttn_option");
+
+  playButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+      playGame(event.currentTarget.id);
+    })
+  });
+
+  const optionsSection = document.querySelector(".interaction__secondary-buttons");
+  const restartGameButton = document.createElement("button");
+  restartGameButton.textContent = "Restart Game";
+  restartGameButton.addEventListener("click", restartGame);
+  restartGameButton.classList.add("bttn_option", "bttn_restart")
+
   
-  const validAnswers = ["rock", "paper", "scissors"];
-  let userInput;
+  function getComputerChoice() {
+    const options = ["rock", "paper", "scissors"];
+    const indexPosition = Math.floor((Math.random() * options.length));
+    return options[indexPosition];
+  }
 
-  do {
-    let rawInput = prompt("Select rock, paper or scissors");
-    // If user pressed "cancel", return an empty string
-    userInput = rawInput ? rawInput.toLowerCase() : "";
-
-  } while (!userInput || !validAnswers.includes(userInput));
-
-  return userInput;
-}
-
-function playRound(humanChoice, computerChoice) {
+  /** @param {string} userChoice 
+   * @param {string} computerChoice */
+  function playRound(userChoice, computerChoice) {  
   
-  if (humanChoice === computerChoice) return "draw";
+  if (userChoice === computerChoice) return "draw";
+  
+    const winsAgainst = {
 
-  const winsAgainst = {
-    
     rock: "scissors",
     paper: "rock",
     scissors: "paper"
-  };
+    };
 
-  return winsAgainst[humanChoice] === computerChoice ? "human" : "computer";
+    return winsAgainst[userChoice] === computerChoice ? "human" : "computer";
+  }
 
-}
+  /** @param {string} optionSelected */
+  function playGame(optionSelected) {
+    
+    if (endGame) return;
 
-
-
-function playGame() {
-
-  let humanScore = 0;
-  let computerScore = 0;
-
-  for (let i = 1; i <= 5; i++) {
-  
-    const humanChoice = getUserChoice();
+    const userChoice = optionSelected;
     const computerChoice = getComputerChoice();
 
-    const winner = playRound(humanChoice, computerChoice);
+    const winner = playRound(userChoice, computerChoice);
+    checkWinner(winner, userChoice, computerChoice);
+    updateIcons(userChoice, computerChoice)
 
-    if (winner === "draw") {
-      alert("It's a draw!");
-      i--;
-      continue;
+    if (userScore === 5 || computerScore === 5) {   
+      endGame = true;
+      gameOver();
     }
     
-    if (winner === "human") {
-      alert(`You win this round! ${humanChoice} beats ${computerChoice}`);
-      humanScore++;
+    
+  }
 
+  /** @param {string} winner  
+   * @param {string} userChoice 
+   * @param {string} computerChoice 
+  */
+  function checkWinner(winner, userChoice, computerChoice) {
+    if (winner === "draw") {
+      updateOutput("It's a draw!")
+      
+    } else if(winner === "human") {  
+      updateOutput(`You win this round! ${userChoice} beats ${computerChoice}`)
+      userScore++;
     } else {
-      alert(`You lost this round! ${computerChoice} beats ${humanChoice}`);
+      updateOutput(`You lost this round! ${computerChoice} beats ${userChoice}`)
       computerScore++;
+    }
+
+
+    
+
+    updateScoreText();
+  }
+
+  function gameOver() {
+    const winnerMessage = userScore > computerScore
+      ? `You win matey!`
+      : `You lose! Good luck next time!`
+      
+    updateOutput(winnerMessage);
+    optionsSection?.appendChild(restartGameButton);  
+  }
+
+    
+  /** @param {string} text  */
+  function updateOutput(text) {  
+    if (outputText) {
+      outputText.textContent = text;
     }
   }
 
-  const winnerMessage = humanScore > computerScore
-  ? `You win! Your Score: ${humanScore} | Computer Score: ${computerScore}`
-  : `You lose! Your Score: ${humanScore} | Computer Score: ${computerScore}`
+  function updateScoreText() {
+    if (userScoreText && computerScoreText) {
+      userScoreText.textContent = `User Score: ${userScore}`;
+      computerScoreText.textContent = `Computer Score: ${computerScore}`;
+    }
+  }
 
-  alert(winnerMessage);
+  function  restartGame() {
 
-}
+    userScore = 0;
+    computerScore = 0;
+    updateScoreText();
 
-playGame();
+    endGame = false;
+    updateOutput("Please, select one of the available options!")
+
+    if (userIcon && computerIcon) {
+      const emptyIcon = "▉ ▊ ▋ ▌ ▍ ▎ ▏ ▎ ▍ ▌ ▋ ▊ ▉";
+      userIcon.textContent = emptyIcon;
+      computerIcon.textContent = emptyIcon; 
+     }
+
+    restartGameButton.remove();
+  }
+
+
+  /** @param {string} userChoice
+  * @param {string} computerChoice 
+  */
+  function updateIcons(userChoice, computerChoice) {
+    const ascciIcons = {
+      rock:
+`
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+`,
+      paper:
+`_______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+`,
+      scissors:
+`_______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+`,
+      }
+
+
+      if (userIcon && computerIcon) {  
+        userIcon.textContent = ascciIcons[userChoice];
+        computerIcon.textContent = ascciIcons[computerChoice];  
+      }    
+  }
+})();
