f97923ea481aeca48a10293f76583db1fa06fa87
sally <sally@sall.ipv6.infinitum.net.mx>
2026-06-13 19:45:24 -0600

    feat(js): finish RPS game logic

--- js/main.js
+++ js/main.js
@@ -2,29 +2,39 @@
 
 ( () => {
   
-  // Scores
+  // game logic
   let userScore = 0;
   let computerScore = 0;
+  let endGame = false;
 
   // DOM elements
-  const playButtons = document.querySelectorAll(".bttn_option");
   const outputText = document.querySelector(".rps__output-text");
+  const userIcon = document.querySelector('[data-icon="user"]')
+  const computerIcon = document.querySelector('[data-icon="computer"]')
+  const userScoreText = document.querySelector('[data-score="user"]')
+  const computerScoreText = document.querySelector('[data-score="computer"]')
+
+
+  const playButtons = document.querySelectorAll(".bttn_option");
 
   playButtons.forEach(button => {
     button.addEventListener("click", (event) => {
       playGame(event.currentTarget.id);
     })
   });
+
+  const optionsSection = document.querySelector(".interaction__secondary-buttons");
+  const restartGameButton = document.createElement("button");
+  restartGameButton.textContent = "Restart Game";
+  restartGameButton.addEventListener("click", restartGame);
+  restartGameButton.classList.add("bttn_option", "bttn_restart")
+
   
   function getComputerChoice() {
     const options = ["rock", "paper", "scissors"];
     const indexPosition = Math.floor((Math.random() * options.length));
     return options[indexPosition];
   }
-
-  // TODO: Refactorizar getUserChoice() para interacuar con el DOM
-  // Quizá al presionar una opción, ejecutar playGame() y enviar el valor del botón
-  // TODO: Asignar a cada botón la función getUserChoice()
 
   /** @param {string} userChoice 
    * @param {string} computerChoice */
@@ -45,53 +55,124 @@
   /** @param {string} optionSelected */
   function playGame(optionSelected) {
     
-    // TODO: Adaptar para recibir respuesta del DOM
-    const humanChoice = optionSelected;
+    if (endGame) return;
+
+    const userChoice = optionSelected;
     const computerChoice = getComputerChoice();
-    let endGame = false;
 
-    if (userScore >= 5 || computerScore >= 5) {   
+    const winner = playRound(userChoice, computerChoice);
+    checkWinner(winner, userChoice, computerChoice);
+    updateIcons(userChoice, computerChoice)
+
+    if (userScore === 5 || computerScore === 5) {   
       endGame = true;
-    }
-
-    if (!endGame) {
-      const winner = playRound(humanChoice, computerChoice);
-      checkWinner(winner);
-    } else {
       gameOver();
     }
-
-    /** @param {string} winner  */
-    function checkWinner(winner) {
     
-      if (winner === "draw") {
-        updateOutput("It's a draw!")
+    
+  }
 
-      } else if(winner === "human") {  
-        updateOutput(`You win this round! ${humanChoice} beats ${computerChoice}`)
-        userScore++;
-
-      } else {
-        updateOutput(`You lost this round! ${computerChoice} beats ${humanChoice}`)
-        computerScore++;
-      }
-    }
-
-    function gameOver() {
-      const winnerMessage = userScore > computerScore
-        ? `You win! Your Score: ${userScore} | Computer Score: ${computerScore}`  
-        : `You lose! Your Score: ${userScore} | Computer Score: ${computerScore}`
+  /** @param {string} winner  
+   * @param {string} userChoice 
+   * @param {string} computerChoice 
+  */
+  function checkWinner(winner, userChoice, computerChoice) {
+    if (winner === "draw") {
+      updateOutput("It's a draw!")
       
-      updateOutput(winnerMessage);
+    } else if(winner === "human") {  
+      updateOutput(`You win this round! ${userChoice} beats ${computerChoice}`)
+      userScore++;
+    } else {
+      updateOutput(`You lost this round! ${computerChoice} beats ${userChoice}`)
+      computerScore++;
     }
 
-    /** @param {string} text  */
-    function updateOutput(text) {
-      if (outputText) {
-        outputText.textContent = text;
-      }
-    }
 
     
+
+    updateScoreText();
+  }
+
+  function gameOver() {
+    const winnerMessage = userScore > computerScore
+      ? `You win matey!`
+      : `You lose! Good luck next time!`
+      
+    updateOutput(winnerMessage);
+    optionsSection?.appendChild(restartGameButton);  
+  }
+
+    
+  /** @param {string} text  */
+  function updateOutput(text) {  
+    if (outputText) {
+      outputText.textContent = text;
+    }
+  }
+
+  function updateScoreText() {
+    if (userScoreText && computerScoreText) {
+      userScoreText.textContent = `User Score: ${userScore}`;
+      computerScoreText.textContent = `Computer Score: ${computerScore}`;
+    }
+  }
+
+  function  restartGame() {
+
+    userScore = 0;
+    computerScore = 0;
+    updateScoreText();
+
+    endGame = false;
+    updateOutput("Please, select one of the available options!")
+
+    if (userIcon && computerIcon) {
+      const emptyIcon = "▉ ▊ ▋ ▌ ▍ ▎ ▏ ▎ ▍ ▌ ▋ ▊ ▉";
+      userIcon.textContent = emptyIcon;
+      computerIcon.textContent = emptyIcon; 
+     }
+
+    restartGameButton.remove();
+  }
+
+
+  /** @param {string} userChoice
+  * @param {string} computerChoice 
+  */
+  function updateIcons(userChoice, computerChoice) {
+    const ascciIcons = {
+      rock:
+`
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+`,
+      paper:
+`_______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+`,
+      scissors:
+`_______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+`,
+      }
+
+
+      if (userIcon && computerIcon) {  
+        userIcon.textContent = ascciIcons[userChoice];
+        computerIcon.textContent = ascciIcons[computerChoice];  
+      }    
   }
 })();
